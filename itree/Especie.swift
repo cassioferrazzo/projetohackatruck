7a6ee3b9b944807b66c0c4966ffe969a2c85c120
Cássio Ferrazzo <cassioferrazzo@gmail.com>
2016-09-27 16:53:34 -0300

    Telas iniciais OK

--- itree/Especie.swift
+++ itree/Especie.swift
@@ -10,16 +10,14 @@ import Foundation
 class Especie{
     
     let nome: String
-    let nomeGenerico: String
     let nomeAutor: String
     let familia: Familia
     var nomePopular1: String? = nil
     var nomePopular2: String? = nil
     
-    init(nome: String, nomeGenerico: String, nomeAutor: String, familia: Familia){
+    init(nome: String, nomeAutor: String, familia: Familia){
         
         self.nome = nome
-        self.nomeGenerico = nomeGenerico
         self.nomeAutor = nomeAutor
         self.familia = familia
     }
--- itree/ViewController.swift
+++ itree/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tfNomePopular: UITextField!
+    @IBOutlet weak var tfEspecie: UITextField!
+    @IBOutlet weak var tfFamilia: UITextField!
+    @IBOutlet weak var tfFormaDeVida: UITextField!
+    
+    static var formaDeVidaNome: String?
+    static var familiaNome: String?
+    static var especieNome: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier! == "segueSelectFamilia" {
+            if let nextSegue = segue.destinationViewController as? SelectFamiliaTableViewController {
+                if let formaVida = tfFormaDeVida.text {
+                    nextSegue.formaDeVida = FormaVida(nome: formaVida , descricao: "")
+                }
+            }
+            
+        } else if segue.identifier! == "segueSelectEspecie"{
+            if let nextSegue = segue.destinationViewController as? SelectEspecieTableViewController {
+                if let familia = tfFamilia.text {
+                    nextSegue.familia = Familia(nome: familia, formaVida:  FormaVida(nome: familia , descricao: ""))
+                }
+            }
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        if let familia = ViewController.familiaNome {
+            tfFamilia.text = familia
+        }
+        if let formaVida = ViewController.formaDeVidaNome {
+            tfFormaDeVida.text = formaVida
+        }
+        if let especie = ViewController.especieNome {
+            tfEspecie.text = especie
+        }
+    }
 }
 
